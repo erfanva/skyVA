c7169fd99ad19c99f1b8354fded7c951b5e43694
e.vahabi <e.vahabi@viratech.ir>
2018-08-08 15:39:09 +0430

    add clouds

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var initialDeg = -30;
+var initialLeft = 100;
+
 jQuery.fn.rotate = function(degrees) {
     $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
                  '-moz-transform' : 'rotate('+ degrees +'deg)',
@@ -7,13 +10,39 @@ jQuery.fn.rotate = function(degrees) {
 };
 
 $(document).ready(function(){
+    var top = $(window).scrollTop();
+    var maxTop = $(document).height() - $(window).height();
+    var ratio = top/maxTop;
+    
+    // Sun & Moon
+    var angle = (180 - 2*initialDeg) * ratio + initialDeg;
+    $('#skyva-sm-container').height(2*$(window).height());
+    $('#skyva-sm-container').rotate(angle);
+
+    // Clouds
+    var left = ($(window).width() - 2*initialLeft) * ratio + initialLeft;
+    $('#skyva-cloud-container').css({left: left + "px"});
+    console.log("(angle:", angle, "left:", left,")");
+
+    // Light
+    $('#skyva-light-container').css('opacity', ratio*0.5);
+    
     $(window).scroll(function() {
-        var top = $(window).scrollTop();
-        var maxTop = $(document).height() - $(window).height();
-        var ratio = top/maxTop;
-        var angle = 180 * ratio;
-        $('#container').height(2*$(window).height());
-        $('#container').rotate(angle);
-        console.log(angle);
+        top = $(window).scrollTop();
+        maxTop = $(document).height() - $(window).height();
+        ratio = top/maxTop;
+
+        // Sun & Moon
+        var angle = (180 - 2*initialDeg) * ratio + initialDeg;
+        $('#skyva-sm-container').height(2*$(window).height());
+        $('#skyva-sm-container').rotate(angle);
+
+        // Clouds
+        var left = ($(window).width() - 2*initialLeft) * ratio + initialLeft;
+        $('#skyva-cloud-container').css({left: left + "px"});
+        console.log("(angle:", angle, "left:", left,")");
+
+        // Light
+        $('#skyva-light-container').css('opacity', ratio*0.5);
     });
 });
