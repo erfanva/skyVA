70afb02f1f86c55fec4d3b642ddf0d8aba02355b
e.vahabi <e.vahabi@viratech.ir>
2018-08-08 13:13:12 +0430

    add background and picture

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
         var top = $(window).scrollTop();
         var maxTop = $(document).height() - $(window).height();
         var ratio = top/maxTop;
-        var angle = 360 * ratio;
-        $('#container').height($(window).height());
+        var angle = 180 * ratio;
+        $('#container').height(2*$(window).height());
         $('#container').rotate(angle);
         console.log(angle);
     });
